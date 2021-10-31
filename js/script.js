2dbdea563165d2c927a1461b2db7fd34856cca46
ramzan-gladiator <81308595+ramzan-gladiator@users.noreply.github.com>
2021-10-31 22:06:10 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+ const menuBtn = document.querySelector(".menu-btn");
+ const burger = document.querySelector(".menu-btn__burger");
+ const nav = document.querySelector(".nav");
+ const menuNav = document.querySelector(".menu-nav");
+ const navItem = document.querySelectorAll(".menu-nav__item");
+
+ let showMenu = false;
+
+ menuBtn.addEventListener("click", toggleMenu);
+
+ function toggleMenu(){
+  if(!showMenu){
+    burger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItem.forEach(item => item.classList.add('open'));
+    showMenu = true;
+  }
+  else{
+    burger.classList.remove('open');  
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItem.forEach(item => item.classList.remove('open'));
+    showMenu = false;
+  }
+}
